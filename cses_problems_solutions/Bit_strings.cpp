c9a376cab258f0eea899c12e7894f5c7704fb881
Ikhtiyor31 <55629713+Ikhtiyor31@users.noreply.github.com>
2020-09-30 14:40:46 +0900

    Create Bit_strings.cpp
    https://cses.fi/problemset/task/1617/

---
+++ cses_problems_solutions/Bit_strings.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+const int MOD = 1e9 + 7;
+using namespace std;
+ll maxn = 1e5 + 5;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    ll ans = 1;
+    for(int i = 0; i < n; i++) {
+    	ans = ans * 2 % MOD;
+    }
+     cout << ans << "\n";
+	return 0;
+}
